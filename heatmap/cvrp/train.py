40a8b78571f8ae2d1281886282901e2d6861a241
muk-jjang <whgdk0911@khu.ac.kr>
2025-01-18 02:35:16 -0500

    fix: sparse params, method paths

--- heatmap/cvrp/train.py
+++ heatmap/cvrp/train.py
@@ -3,48 +3,30 @@ sys.path.insert(0, './')
 import torch
 import time
 import argparse
-import numpy as np
-import math
-import scipy.special
-import wandb
 from tqdm import tqdm
 from heatmap.cvrp.inst import gen_inst, gen_pyg_data, trans_tsp
 from heatmap.cvrp.eval import eval
 from heatmap.cvrp.sampler import Sampler
 from nets.partition_net import Net
 from utils import load_model
+import wandb
 
 EPS = 1e-10
 LR = 3e-4
 K_SPARSE = {
-    1000: 100,
-    2000: 200,
+    200: 40,
+    500: 100,
+    1000: 200,
+    2000: 400,
 }
 
-##TODO: sanghyeok backward policy
-def calculate_log_pb_uniform(paths: torch.Tensor):
-    # paths.shape: (batch, max_tour_length)
-    # paths are start with 0 and end with 0
-
-    _pi1 = paths.detach().cpu().numpy()
-    # shape: (batch, max_tour_length)
-
-    n_nodes = np.count_nonzero(_pi1, axis=1)
-    _pi2 = _pi1[:, 1:] - _pi1[:, :-1]
-    n_routes = np.count_nonzero(_pi2, axis=1) - n_nodes # count the number of routes in each instance
-    _pi3 = _pi1[:, 2:] - _pi1[:, :-2]
-    n_multinode_routes = np.count_nonzero(_pi3, axis=1) - n_nodes
-    log_b_p = - scipy.special.gammaln(n_routes + 1) - n_multinode_routes * math.log(2)
-
-    return torch.from_numpy(log_b_p).to(paths.device)
-
-def infer_heatmap(model, pyg_data, gfn_loss):
-    heatmap, logZ = model(pyg_data, gfn_loss)
+def infer_heatmap(model, pyg_data):
+    heatmap = model(pyg_data)
     heatmap = heatmap / (heatmap.min()+1e-5)
     heatmap = model.reshape(pyg_data, heatmap) + 1e-5
-    return heatmap, logZ
+    return heatmap
     
-def train_batch(model, optimizer, n, bs, opts, beta, it):
+def train_batch(model, optimizer, n, bs, opts, it):
     model.train()
     loss_lst = []
     ##################################################
@@ -52,57 +34,33 @@ def train_batch(model, optimizer, n, bs, opts, beta, it):
     _train_mean_cost = 0.0
     _train_min_cost = 0.0
     _train_max_cost = 0.0
-    _logZ_mean = torch.tensor(0.0, device=DEVICE)
-    sum_loss = torch.tensor(0.0, device=DEVICE)
     count = 0
     ##################################################
     for _ in range(opts.batch_size):
-        coors, demand, capacity = gen_inst(n, DEVICE)# coors: (n+1, 2), tensor  demand: (n+1, ), tensor  capacity: float
-        pyg_data = gen_pyg_data(coors, demand, capacity, K_SPARSE[n])# pyg_data: PyGData
-        heatmap, logZ = infer_heatmap(model, pyg_data, opts.gfn_loss)# heatmap: (n+1, n+1)
+        coors, demand, capacity = gen_inst(n, DEVICE)
+        pyg_data = gen_pyg_data(coors, demand, capacity, K_SPARSE[n])
+        heatmap = infer_heatmap(model, pyg_data)
         sampler = Sampler(demand, heatmap, capacity, bs, DEVICE)
-        routes, log_probs = sampler.gen_subsets(require_prob=True) # routes: (bs, trajectories), log_probs: (bs, trajectories)
-        tsp_insts, n_tsps_per_route = trans_tsp(coors, routes)
-        objs = eval(tsp_insts, n_tsps_per_route, opts) # (bs, )
-        
-        ##TODO: vargrad, tb 구성, off-policy buffer
-        if opts.gfn_loss == 'vargrad':
-            log_pf = log_probs.to(DEVICE).sum(dim=1)
-            log_pb = calculate_log_pb_uniform(routes)
-            zeta = -beta*objs + log_pb - log_pf
-            gfn_loss = torch.pow(zeta - zeta.mean(),2).mean() 
-        elif opts.gfn_loss == 'tb':
-            log_pf = log_probs.to(DEVICE).sum(dim=1)
-            log_pb = calculate_log_pb_uniform(routes)
-            forward_flow = log_pf + logZ.expand(log_probs.size(0))
-            backward_flow = log_pb - beta*objs
-            gfn_loss = torch.pow(forward_flow - backward_flow, 2).mean()
-        
-        loss_lst.append(gfn_loss)
+        routes, log_probs = sampler.gen_subsets(require_prob=True)
+        # tsp_insts:tsp instance
+        # n_tsps_per_route: sub-tour 개수
+        tsp_insts, n_tsps_per_route = trans_tsp(coors, routes) # CVRP 샘플링 한거 -> sub-tour로 변환
+        objs = eval(tsp_insts, n_tsps_per_route, opts) ##TODO LKH3로 하는 부분 추가해야함
+        baseline = objs.mean()
+        log_probs = log_probs.to(DEVICE)
+        reinforce_loss = torch.sum((objs-baseline) * log_probs.sum(dim=1)) / bs
+        loss_lst.append(reinforce_loss)
         count += 1
 
-        max_obj_index = objs.argmax()
-        min_obj_index = objs.argmin()
-        
         ##################################################
         # wandb
         if USE_WANDB:
             _train_mean_cost += objs.mean().item()
             _train_min_cost += objs.min().item()
             _train_max_cost += objs.max().item()
-            _logZ_mean += logZ.item()
-            
-            instance_step = it * opts.batch_size + count - 1
-            wandb.log({'instance':{
-                'max_obj_sample_instance': objs[max_obj_index].item(),
-                'max_obj_sample_logpf': log_pf[max_obj_index].item(),
-                'max_obj_sample_logpb': log_pb[max_obj_index].item(),
-                'min_obj_sample_instance': objs[min_obj_index].item(),
-                'min_obj_sample_logpf': log_pf[min_obj_index].item(),
-                'min_obj_sample_logpb': log_pb[min_obj_index].item(),
-            }}, step=instance_step)
         ##################################################
 
+    
     loss = sum(loss_lst) / opts.batch_size
     optimizer.zero_grad()
     loss.backward()
@@ -119,8 +77,6 @@ def train_batch(model, optimizer, n, bs, opts, beta, it):
                 "train_min_cost": _train_min_cost / count,
                 "train_max_cost": _train_max_cost / count,
                 "train_loss": loss.item(),
-                "logZ": _logZ_mean.item() / count,
-                "beta": beta,
             }},
             step=it,
         )
@@ -130,24 +86,18 @@ def infer_instance(model, inst, opts):
     coors, demand, capacity = inst
     n = demand.size(0)-1
     pyg_data = gen_pyg_data(coors, demand, capacity, K_SPARSE[n])
-    heatmap, logZ = infer_heatmap(model, pyg_data, opts.gfn_loss)
+    heatmap = infer_heatmap(model, pyg_data)
     sampler = Sampler(demand, heatmap, capacity, 1, DEVICE)
     routes = sampler.gen_subsets(require_prob=False, greedy_mode=True)
     tsp_insts, n_tsps_per_route = trans_tsp(coors, routes)
     obj = eval(tsp_insts, n_tsps_per_route, opts).min()
     return obj
 
-def train_epoch(n, bs, steps_per_epoch, net, optimizer, scheduler, opts, beta_schedule_params, epoch, n_epochs):
-    #Beta Schedule
-    # beta_min, beta_max, beta_flat_epochs = beta_schedule_params
-    # beta = beta_min + (beta_max - beta_min) * min(math.log(epoch) / math.log(n_epochs - beta_flat_epochs), 1.0)
-    beta = beta_schedule_params
+def train_epoch(n, bs, steps_per_epoch, net, optimizer, scheduler, opts, epoch, n_epochs):
     for i in tqdm(range(steps_per_epoch)):
         it = (epoch - 1) * steps_per_epoch + i
-        train_batch(net, optimizer, n, bs, opts, beta, it)
+        train_batch(net, optimizer, n, bs, opts, it)
     scheduler.step()
-    
-    
 
 @torch.no_grad()
 def validation(n, net, opts):
@@ -160,7 +110,7 @@ def validation(n, net, opts):
     best_obj = torch.tensor(sum_obj).min()
     return avg_obj, best_obj
 
-def train(n, bs, steps_per_epoch, n_epochs, opts, beta_schedule_params=(50, 500, 5)):
+def train(n, bs, steps_per_epoch, n_epochs, opts):
     revisers = []
     for reviser_size in opts.revision_lens:
         reviser_path = f'pretrained/Reviser-stage2/reviser_{reviser_size}/epoch-299.pt'
@@ -172,7 +122,7 @@ def train(n, bs, steps_per_epoch, n_epochs, opts, beta_schedule_params=(50, 500,
         reviser.set_decode_type(opts.decode_strategy)    
     opts.revisers = revisers
     
-    net = Net(opts.units, 3, K_SPARSE[n], 2, depth=opts.depth, gfn_loss=opts.gfn_loss).to(DEVICE)
+    net = Net(opts.units, 3, K_SPARSE[n], 2, depth=opts.depth).to(DEVICE)
     optimizer = torch.optim.AdamW(net.parameters(), lr=LR)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=n_epochs)
     
@@ -190,7 +140,7 @@ def train(n, bs, steps_per_epoch, n_epochs, opts, beta_schedule_params=(50, 500,
     print('epoch 0', best_avg_obj.item())
     for epoch in range(starting_epoch, n_epochs + 1):
         start = time.time()
-        train_epoch(n, bs, steps_per_epoch, net, optimizer, scheduler, opts, beta_schedule_params, epoch, n_epochs)
+        train_epoch(n, bs, steps_per_epoch, net, optimizer, scheduler, opts, epoch, n_epochs)
         sum_time += time.time() - start
         avg_obj, best_obj = validation(n, net, opts)
         print(f'epoch {epoch}: ', avg_obj.item())
@@ -204,13 +154,13 @@ def train(n, bs, steps_per_epoch, n_epochs, opts, beta_schedule_params=(50, 500,
                 'scheduler_state_dict': scheduler.state_dict(),
             }
             torch.save(checkpoint, f'./pretrained/Partitioner/cvrp/cvrp-{n}-{epoch}-cos.pt')
-        
+            
         # wandb
         wandb.log({"val":{
             'val_avg_obj': avg_obj.item(),
             'val_best_obj': best_obj.item(),
             'epoch': epoch,
-        }}, step=epoch)
+        }}, step=epoch*steps_per_epoch)
     print('total training duration:', sum_time)
     
 if __name__ == '__main__':
@@ -234,14 +184,11 @@ if __name__ == '__main__':
     parser.add_argument('--no_clip', action='store_true')
     parser.add_argument('--batch_size', type=int, default=10)
     parser.add_argument('--depth', type=int, default=12)
-    #GFN loss: tb, vargrad
-    parser.add_argument('--gfn_loss', type=str, default='tb', help='vargrad or tb')
-    #beta
-    parser.add_argument('--beta', type=float, default=200)
     #logging
     parser.add_argument("--disable_wandb", action="store_true", help="Disable wandb logging")
     parser.add_argument("--run_name", type=str, default="", help="Run name")
-    #multi-gpu
+
+    #multi gpu
     parser.add_argument('--gpu', type=int, default=0)
 
     opts = parser.parse_args()
@@ -253,8 +200,8 @@ if __name__ == '__main__':
     # wandb setting
     USE_WANDB = not opts.disable_wandb
 
-    run_name = opts.run_name if opts.run_name != "" else f"GFN-{opts.problem_size}"
-    run_name += f"{opts.gfn_loss}-n{opts.problem_size}-sample_per_instance{opts.width}-beta{opts.beta}-sd{opts.seed}"
+    run_name = opts.run_name if opts.run_name != "" else f"GLOP-{opts.problem_size}"
+    run_name += f"n{opts.problem_size}-s{opts.width}-sd{opts.seed}"
     if USE_WANDB:
         wandb.init(project=f"glopgfn-cvrp", name=run_name, entity='glop-gfn')
         wandb.config.update(opts)
@@ -262,7 +209,7 @@ if __name__ == '__main__':
 
     # Device
     DEVICE = f'cuda:{opts.gpu}' if torch.cuda.is_available() else 'cpu'
-
+    
     torch.manual_seed(opts.seed)
     pp.pprint(vars(opts))
-    train(opts.problem_size, opts.width, opts.steps_per_epoch, opts.n_epochs, opts, beta_schedule_params=opts.beta)
+    train(opts.problem_size, opts.width, opts.steps_per_epoch, opts.n_epochs, opts)
